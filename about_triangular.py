5405be86614e8b140c10e8478ee0c4fdb10b7879
ze zhang <1120180303@bit.edu.cn>
2023-05-23 18:26:04 +0800

    增加了about_triangular.py用来验证三角分布，其中对整数列表计数并排序的程序比较有意思。

---
+++ about_triangular.py
@@ -0,0 +1,45 @@
+import random
+import matplotlib.pyplot as plt
+
+
+LOW = 0
+HIGH = 9
+MODE = [2, 4, 6]
+
+NUMS = 10000
+
+
+def getOneTrianguler(low, high, mode):
+
+    return round(random.triangular(low, high, mode))
+
+
+def myCount(result):
+
+    newResult = {}
+
+    for r in result:
+        if r in newResult.keys():
+            newResult[r] += 1
+        else:
+            newResult[r] = 1
+
+    newResult = dict(sorted(newResult.items(), key=lambda x: x[0]))
+
+    return list(newResult.keys()), list(newResult.values())
+
+
+result = [[],[],[]]
+for i in range(NUMS):
+    result[0].append(getOneTrianguler(LOW, HIGH, MODE[0]))
+    result[1].append(getOneTrianguler(LOW, HIGH, MODE[1]))
+    result[2].append(getOneTrianguler(LOW, HIGH, MODE[2]))
+
+
+plt.plot(*myCount(result[0]), label=f"mode={MODE[0]}")
+plt.plot(*myCount(result[1]), label=f"mode={MODE[1]}")
+plt.plot(*myCount(result[2]), label=f"mode={MODE[2]}")
+
+plt.legend()
+plt.show()
+
