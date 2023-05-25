7415717be3cd1936f4ab6362bfb86b84ba4d7f09
ze zhang <1120180303@bit.edu.cn>
2023-05-25 16:37:24 +0800

    为整个项目增加了注释，修正了找到目标之后地图显示的问题。

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from search import Search
 
 def printMenu():
 
+    # 打印菜单供用户交互使用，并收集用户在该次交互中的指令;
     result = []
 
     result.append(input("是否放弃搜索: "))
@@ -17,6 +18,7 @@ def printMenu():
 
 def printPAndE(P, E):
 
+    # 格式化打印;
     for i in range(len(P)):
         print(f"p{i+1}={P[i]:.2f}", end="  ")
 
@@ -30,6 +32,8 @@ def printPAndE(P, E):
 def main():
 
     task = Search()
+
+    # 初始化船员位置;
     task.getSailorPosition()
 
     day = 0
@@ -39,27 +43,35 @@ def main():
 
         day += 1
 
-
+        # 生成当天各个搜索区域预期的搜索有效性，格式化打印概率与SEP信息;
         task.SEPWithWeather()
         printPAndE(task.p, task.e)
+
+        # 显示地图，地图中给出了当天各个区域的天气情况以及上一次搜索的区域（绿色标注）;
         task.getMap(lastRegion)
 
+        # 开始当天的搜索决策;
         result = printMenu()
 
         regionOne = result[1]
         regionTwo = result[2]
 
+        # 记录当天的搜索决策，用于下一次搜索时地图上的区域标注;
         lastRegion = [regionOne, regionTwo]
 
+        # 执行搜索（这里会产生搜索结果，记录在Search.flag中）;
         searchRegionOne = task.search(regionOne)
         searchRegionTwo = task.search(regionTwo)
 
+        # 先更新当天进行的两次搜索的实际SEP结果;
         if regionOne == regionTwo:
             for j in range(len(task.e)):
                 if j == (regionOne - 1):
+                    # 两次搜索在同一位置进行，需要计算新的SEP（参考README.md）;
                     task.e[j] = (len(set(searchRegionOne + searchRegionTwo))) / \
                                 ((task.searchRegion[0].shape[0] * task.searchRegion[0].shape[1])**2)
                 else:
+                    # 将其余区域的SEP还原为0;
                     task.e[j] = 0
         else:
             for j in range(len(task.e)):
@@ -68,13 +80,16 @@ def main():
                 elif j == (regionTwo - 1):
                     pass
                 else:
+                    # 将其余区域的SEP还原为0;
                     task.e[j] = 0
 
         if task.flag == True:
             print(f"耗时{day}天成功找到目标，其坐标位于: {task.sailor}.")
-            task.getMap([searchRegionOne, searchRegionTwo])
+            # 找到之后更新一下地图;
+            task.getMap(lastRegion)
             sys.exit()
         else:
+            # 根据实际搜索的SEP重新计算下一次搜索的概率并更新在Search类中;
             task.getNewP()
             print(f"在第{day}天时未能成功找到目标.")
 
